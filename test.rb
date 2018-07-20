f62a8001e309dc1415c7ef31f4e180987acd107c
Tatsuro Baba <harakirisoul@gmail.com>
2018-07-20 14:19:24 +0900

    wip

--- test.rb
+++ test.rb
@@ -5,6 +5,7 @@ class Person
   include ActiveModel::AttributeMethods
 
   attribute_method_prefix 'clear_'
+  attribute_method_suffix '_was'
   define_attribute_methods :name
 
   attr_accessor :name
@@ -13,7 +14,22 @@ class Person
     { name: @name }
   end
 
+  def name=(val)
+    cached_attributes.merge!(name: @name)
+    @name = val
+  end
+
+  private
+
   def clear_attribute(attr)
     send("#{attr}=", nil)
+  end
+
+  def attribute_was(attr)
+    @cached_attributes[attr]
+  end
+
+  def cached_attributes
+    @cached_attributes = self.attributes
   end
 end
