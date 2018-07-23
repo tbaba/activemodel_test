c4fb9ea692fc55581f6aabb3ea946442ee4a8d18
Tatsuro Baba <harakirisoul@gmail.com>
2018-07-23 15:10:58 +0900

    Implement attribute_was

--- test.rb
+++ test.rb
@@ -1,35 +1,39 @@
+require 'active_support/hash_with_indifferent_access'
 require 'active_model'
-require 'pry'
 
 class Person
   include ActiveModel::AttributeMethods
 
-  attribute_method_prefix 'clear_'
   attribute_method_suffix '_was'
   define_attribute_methods :name
 
   attr_accessor :name
+
+  def initialize(name: nil)
+    @name = name
+    @cached_attributes = ActiveSupport::HashWithIndifferentAccess.new(name: @name)
+  end
 
   def attributes
     { name: @name }
   end
 
   def name=(val)
-    cached_attributes.merge!(name: @name)
+    cache_changed_attributes(:name, @name)
     @name = val
   end
 
   private
 
-  def clear_attribute(attr)
-    send("#{attr}=", nil)
+  def cache_changed_attributes(attr, val)
+    cached_attributes[attr] = val
+  end
+
+  def cached_attributes
+    @cached_attributes ||= ActiveSupport::HashWithIndifferentAccess.new
   end
 
   def attribute_was(attr)
     @cached_attributes[attr]
-  end
-
-  def cached_attributes
-    @cached_attributes = self.attributes
   end
 end
